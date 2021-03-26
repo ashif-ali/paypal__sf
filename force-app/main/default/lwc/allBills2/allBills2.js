f32fd663f7bfa903f41e30a0f945717636bb690f
ashif-ali <ashifali2327@gmail.com>
2021-03-26 14:12:59 +0530

    implemented updateRecord

--- force-app/main/default/lwc/allBills2/allBills2.js
+++ force-app/main/default/lwc/allBills2/allBills2.js
@@ -1,20 +1,44 @@
 import { LightningElement, api, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getBills from "@salesforce/apex/BillsController.getBills";
+import { updateRecord } from "lightning/uiRecordApi";
+import PAID from "@salesforce/schema/Bills__c.Paid__c";
+import ID_FIELD from "@salesforce/schema/Bills__c.Id";
 
 const columns = [
   { label: "Name", fieldName: "Name" },
   {
     label: "Payable Amount",
     fieldName: "Final_Payable_Amount__c",
-    type: "currency"
+    type: "currency",
+    cellAttributes: { alignment: "left" }
   },
   {
     label: "Pay To",
-    fieldName: "Pay_To__c"
+    fieldName: "Pay_To_Name__c",
+    type: "text"
   },
   {
     label: "Available Balance",
-    fieldName: "Available_wallet_balance__c"
+    fieldName: "Available_wallet_balance__c",
+    type: "currency",
+    cellAttributes: { alignment: "left" }
+  },
+  {
+    label: "Paid",
+    fieldName: "Paid__c",
+    type: "checkbox",
+    cellAttributes: { alignment: "left" }
+  },
+  {
+    type: "button",
+    typeAttributes: {
+      label: "Pay",
+      name: "pay",
+      title: "Pay",
+      disabled: false,
+      iconPosition: "left"
+    }
   }
 ];
 
@@ -25,6 +49,16 @@ export default class AllBills2 extends LightningElement {
   @track
   billId;
 
+  @track
+  finalPayableAmount;
+
+  @track
+  walletBalance;
+
+  @track
+  paid;
+
+  @track
   datas;
   columns = columns;
 
@@ -33,14 +67,52 @@ export default class AllBills2 extends LightningElement {
       id: this.recordId
     })
       .then((bills) => {
-        // console.log(bills);
-        // this.billId = bills[0].Pay_to__r
-
         this.datas = bills;
-        console.log(bills);
+        // console.log(bills);
+        console.log(this.recordId);
+        console.log(this.datas[1].Id);
       })
       .catch((error) => {
         console.log(error);
       });
+  }
+
+  callRowAction() {
+    let el = this.template.querySelector("lightning-datatable");
+    let selected = el.getSelectedRows();
+    this.finalPayableAmount = selected[0].Final_Payable_Amount__c;
+    this.walletBalance = selected[0].Available_wallet_balance__c;
+    this.paid = selected[0].Paid__c;
+    console.log(this.finalPayableAmount);
+    console.log(this.walletBalance);
+    console.log(this.paid);
+
+    if (this.paid === true) {
+      const evt = new ShowToastEvent({
+        title: "Already Paid",
+        message: "You have already paid this bill",
+        variant: "info"
+      });
+      this.dispatchEvent(evt);
+    }
+
+    if (this.finalPayableAmount > this.walletBalance) {
+      const evt = new ShowToastEvent({
+        title: "error",
+        message: "You dont have enough wallet balance",
+        variant: "error"
+      });
+      this.dispatchEvent(evt);
+    }
+
+    if (this.paid === false && this.finalPayableAmount < this.walletBalance) {
+      this.paid = true;
+      const fields = {};
+      fields[ID_FIELD.fieldApiName] = this.datas[0].Id;
+      fields[PAID.fieldApiName] = this.paid;
+
+      const recordInput = { fields };
+      updateRecord(recordInput).then((record) => console.log(record));
+    }
   }
 }
